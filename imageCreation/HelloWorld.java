2ea4b6a4082f941de274ac8260d7c7a0520272c9
gowthamReddy <gowthamreddy.6991@gmail.com>
2023-08-22 16:36:29 +0530

    Multistage docker file and Dockerfile along with java file

---
+++ imageCreation/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+public static void main(String[] args) {
+    loop_1_to_10();
+}
+
+// The below method will print the values 1 to 10.
+    public static void loop_1_to_10() {
+        for (int i = 0; i<11 ; i++) {
+            System.out.println("Loop :"+i);
+        }
+    }
+}
